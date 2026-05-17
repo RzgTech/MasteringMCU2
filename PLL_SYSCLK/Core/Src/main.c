caec330ce2e0aaf0c04006a92d8c01a4ff6a276b
Vahid <vahidrspace@gmail.com>
2026-05-17 22:47:04 +0200

    PLL configuration via HSI: part1

--- PLL_SYSCLK/Core/Src/main.c
+++ PLL_SYSCLK/Core/Src/main.c
@@ -15,9 +15,10 @@
 
 void UART2_Init(void);
 void Error_handler(void);
-
+void systemclock_config(uint8_t clocl_freq);
 
 UART_HandleTypeDef huart;
+
 
 
 
@@ -28,14 +29,63 @@ int main()
 
 	UART2_Init();
 
-
+	uint8_t clock_freq;
+	systemclock_config(clock_freq);
 
 	while(1);
 
 	return 0;
 }
 
+void systemclock_config(uint8_t clocl_freq)
+{
+	RCC_OscInitTypeDef osc_init;
+	RCC_ClkInitTypeDef clk_init;
 
+	osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	osc_init.HSIState = RCC_HSI_ON; //by default it is ON, but anyway, we write it
+	osc_init.HSICalibrationValue = 16;  //by default it is 16, but anyway, we write it
+	osc_init.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	osc_init.PLL.PLLState = RCC_PLL_ON;
+
+	switch (clocl_freq)
+	{
+	case SYS_CLOCK_FREQ_50_MHZ:
+	{
+		osc_init.PLL.PLLM = 16;
+		osc_init.PLL.PLLN = 100;
+		osc_init.PLL.PLLP = 2;
+		osc_init.PLL.PLLQ = 2; //default value
+		osc_init.PLL.PLLR = 2; //default value
+		break;
+	}
+	case SYS_CLOCK_FREQ_84_MHZ:
+	{
+		osc_init.PLL.PLLM = 16;
+		osc_init.PLL.PLLN = 168;
+		osc_init.PLL.PLLP = 2;
+		osc_init.PLL.PLLQ = 2; //default value
+		osc_init.PLL.PLLR = 2; //default value
+		break;
+	}
+	case SYS_CLOCK_FREQ_120_MHZ:
+	{
+		osc_init.PLL.PLLM = 16;
+		osc_init.PLL.PLLN = 240;
+		osc_init.PLL.PLLP = 2;
+		osc_init.PLL.PLLQ = 2; //default value
+		osc_init.PLL.PLLR = 2; //default value
+		break;
+	}
+	default:
+		return;
+	}
+
+	if (HAL_RCC_OscConfig(&osc_init)!= HAL_OK)
+	{
+		Error_handler();
+	}
+}
 
 void UART2_Init(void)
 {
