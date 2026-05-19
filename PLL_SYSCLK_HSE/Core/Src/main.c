2468a5e8c6fd14ac4842888399cd36e89ea6c7af
Vahid <vahidrspace@gmail.com>
2026-05-19 22:41:47 +0200

    PLL configuration via HSE

---
+++ PLL_SYSCLK_HSE/Core/Src/main.c
@@ -0,0 +1,164 @@
+/*
+ * main.c
+ *
+ *  Created on: May 8, 2026
+ *      Author: Vahid
+ */
+
+#include "main.h"
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#define FALSE 0
+#define TRUE  1
+
+void UART2_Init(void);
+void Error_handler(void);
+void systemclock_config_HSE(uint8_t clocl_freq);
+
+UART_HandleTypeDef huart;
+
+
+
+
+int main()
+{
+	char msg[100];
+	HAL_Init();
+
+	systemclock_config_HSE(SYS_CLOCK_FREQ_50_MHZ); //we should always do this afetr HAL_Init and before other peripheral usages
+	UART2_Init();
+
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "SYSCLK: %ld\r\n", HAL_RCC_GetSysClockFreq());
+	HAL_UART_Transmit(&huart, (uint8_t *)msg, strlen(msg), HAL_MAX_DELAY);
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "HCLK: %ld\r\n", HAL_RCC_GetHCLKFreq());
+	HAL_UART_Transmit(&huart, (uint8_t *)msg, strlen(msg), HAL_MAX_DELAY);
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "PCLK1: %ld\r\n", HAL_RCC_GetPCLK1Freq());
+	HAL_UART_Transmit(&huart, (uint8_t *)msg, strlen(msg), HAL_MAX_DELAY);
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "PCLK2: %ld\r\n", HAL_RCC_GetPCLK2Freq());
+	HAL_UART_Transmit(&huart, (uint8_t *)msg, strlen(msg), HAL_MAX_DELAY);
+
+
+	while(1);
+
+	return 0;
+}
+
+void systemclock_config_HSE(uint8_t clocl_freq)
+{
+	RCC_OscInitTypeDef osc_init;
+	RCC_ClkInitTypeDef clk_init;
+
+	uint32_t FLatency = 0;
+
+	osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	osc_init.HSEState = RCC_HSE_BYPASS; //by default it is ON, but anyway, we write it
+	osc_init.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	osc_init.PLL.PLLState = RCC_PLL_ON;
+
+	switch (clocl_freq)
+	{
+	case SYS_CLOCK_FREQ_50_MHZ:
+	{
+		osc_init.PLL.PLLM = 8;
+		osc_init.PLL.PLLN = 100;
+		osc_init.PLL.PLLP = 2;
+		osc_init.PLL.PLLQ = 2; //default value
+		osc_init.PLL.PLLR = 2; //default value
+
+		clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+				RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+		clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+		clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+		clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+
+		FLatency = FLASH_ACR_LATENCY_1WS;
+		break;
+	}
+	case SYS_CLOCK_FREQ_84_MHZ:
+	{
+		osc_init.PLL.PLLM = 8;
+		osc_init.PLL.PLLN = 168;
+		osc_init.PLL.PLLP = 2;
+		osc_init.PLL.PLLQ = 2; //default value
+		osc_init.PLL.PLLR = 2; //default value
+
+		clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+				RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+		clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+		clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+		clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+
+		FLatency = FLASH_ACR_LATENCY_2WS;
+		break;
+	}
+	case SYS_CLOCK_FREQ_120_MHZ:
+	{
+		osc_init.PLL.PLLM = 8;
+		osc_init.PLL.PLLN = 240;
+		osc_init.PLL.PLLP = 2;
+		osc_init.PLL.PLLQ = 2; //default value
+		osc_init.PLL.PLLR = 2; //default value
+
+
+		clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+				RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+		clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+		clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		clk_init.APB1CLKDivider = RCC_HCLK_DIV4;
+		clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+
+		FLatency = FLASH_ACR_LATENCY_3WS;
+		break;
+	}
+	default:
+		return;
+	}
+
+	if (HAL_RCC_OscConfig(&osc_init)!= HAL_OK)
+	{
+		Error_handler();
+	}
+
+	if (HAL_RCC_ClockConfig(&clk_init, FLatency))
+	{
+		Error_handler();
+	}
+
+	//Systick configuration
+	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+
+}
+
+void UART2_Init(void)
+{
+	huart.Instance = USART2;
+	huart.Init.BaudRate = 115200;
+	huart.Init.WordLength = UART_WORDLENGTH_8B;
+	huart.Init.StopBits = UART_STOPBITS_1;
+	huart.Init.Parity = UART_PARITY_NONE;
+	huart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart.Init.Mode = UART_MODE_TX_RX;
+
+	if (HAL_UART_Init(&huart) != HAL_OK)
+	{
+		//there is a problem
+		Error_handler();
+	}
+}
+
+void Error_handler(void)
+{
+	while(1);
+}
+
+
