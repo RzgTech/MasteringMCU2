58a6610d8b6077154f147a081db4c84bb613bace
Vahid <vahidrspace@gmail.com>
2026-06-08 23:08:13 +0200

    project creation for CAN Normal Mode exercise

---
+++ CAN_Normal_Mode_2Nodes/Core/Inc/main.h
@@ -0,0 +1,13 @@
+/*
+ * main.h
+ *
+ *  Created on: Jun 8, 2026
+ *      Author: Vahid
+ */
+
+#ifndef INC_MAIN_H_
+#define INC_MAIN_H_
+
+
+
+#endif /* INC_MAIN_H_ */
---
+++ CAN_Normal_Mode_2Nodes/Core/Src/it.c
@@ -0,0 +1,8 @@
+/*
+ * it.c
+ *
+ *  Created on: Jun 8, 2026
+ *      Author: Vahid
+ */
+
+
