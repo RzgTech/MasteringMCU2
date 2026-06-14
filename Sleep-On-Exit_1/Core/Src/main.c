56ea53b9978196c28c8d01ebfd4c2a3e0b9ae34c
Vahid <vahidrspace@gmail.com>
2026-06-14 11:00:59 +0200

    Low Power Mode: Project creation for SLEEPONEXIT feature testing

---
+++ Sleep-On-Exit_1/Core/Src/main.c
@@ -0,0 +1,176 @@
+/*
+ * main.c
+ *
+ *  Created on: Jun 8, 2026
+ *      Author: Vahid
+ */
+
+#include "main.h"
+#include "stm32f4xx_hal.h"
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+
+void SystemCLock_Config_HSE(uint8_t clocl_freq);
+void GPIO_Init(void);
+void UART2_Init();
+void Error_handler(void);
+void Timer6_Init(void);
+
+TIM_HandleTypeDef htimer6;  //timer6 is basic timer
+UART_HandleTypeDef huart;  //UART2 handle
+
+int main()
+{
+	HAL_Init();
+	SystemCLock_Config_HSE(SYS_CLOCK_FREQ_50_MHZ);
+
+	GPIO_Init();
+	UART2_Init();
+
+	Timer6_Init();
+
+	while(1);
+
+	return 0;
+}
+
+void SystemCLock_Config_HSE(uint8_t clocl_freq)
+{
+	RCC_OscInitTypeDef osc_init;
+	RCC_ClkInitTypeDef clk_init;
+
+	uint32_t FLatency = 0;
+
+	osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	osc_init.HSEState = RCC_HSE_BYPASS;
+	osc_init.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	osc_init.PLL.PLLState = RCC_PLL_ON;
+
+	switch (clocl_freq)
+	{
+	case SYS_CLOCK_FREQ_50_MHZ:
+	{
+		osc_init.PLL.PLLM = 4;
+		osc_init.PLL.PLLN = 50;
+		osc_init.PLL.PLLP = RCC_PLLP_DIV2;
+		osc_init.PLL.PLLQ = 2; //default value
+		osc_init.PLL.PLLR = 2; //default value
+
+		clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+				RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+		clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+		clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+		clk_init.APB2CLKDivider = RCC_HCLK_DIV1;
+
+		FLatency = FLASH_ACR_LATENCY_1WS;
+		break;
+	}
+	case SYS_CLOCK_FREQ_84_MHZ:
+	{
+		osc_init.PLL.PLLM = 4;
+		osc_init.PLL.PLLN = 84;
+		osc_init.PLL.PLLP = RCC_PLLP_DIV2;
+		osc_init.PLL.PLLQ = 2; //default value
+		osc_init.PLL.PLLR = 1; //default value
+
+		clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+				RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+		clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+		clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+		clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+
+		FLatency = FLASH_ACR_LATENCY_2WS;
+		break;
+	}
+	case SYS_CLOCK_FREQ_120_MHZ:
+	{
+		osc_init.PLL.PLLM = 4;
+		osc_init.PLL.PLLN = 120;
+		osc_init.PLL.PLLP = RCC_PLLP_DIV2;
+		osc_init.PLL.PLLQ = 4; //default value
+		osc_init.PLL.PLLR = 2; //default value
+
+
+		clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+				RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+		clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+		clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		clk_init.APB1CLKDivider = RCC_HCLK_DIV4;
+		clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+
+		FLatency = FLASH_ACR_LATENCY_3WS;
+		break;
+	}
+	default:
+		return;
+	}
+
+	if (HAL_RCC_OscConfig(&osc_init)!= HAL_OK)
+	{
+		Error_handler();
+	}
+
+	if (HAL_RCC_ClockConfig(&clk_init, FLatency) != HAL_OK)
+	{
+		Error_handler();
+	}
+
+	//Systick configuration
+	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+	HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+void GPIO_Init(void)
+{
+
+
+}
+
+void UART2_Init(void)
+{
+	huart.Instance = USART2;
+	huart.Init.BaudRate = 115200;
+	huart.Init.WordLength = UART_WORDLENGTH_8B;
+	huart.Init.StopBits = UART_STOPBITS_1;
+	huart.Init.Parity = UART_PARITY_NONE;
+	huart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart.Init.Mode = UART_MODE_TX_RX;
+
+	if (HAL_UART_Init(&huart) != HAL_OK)
+	{
+		//there is a problem
+		Error_handler();
+	}
+}
+
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+
+}
+
+
+void Timer6_Init(void)
+{
+	//It should be noted that, Timers drive clock from APB bus via a multiplier.
+	//If the prescaler for APB1 is not 1, multiplicator for the timer is 2, o.w it is 1.
+	htimer6.Instance = TIM6;
+	//time base of 1 sec
+	htimer6.Init.Prescaler = 4999;
+	htimer6.Init.Period = 10000-1;
+	if (HAL_TIM_Base_Init(&htimer6) != HAL_OK)
+	{
+		Error_handler();
+	}
+
+}
+
+void Error_handler(void)
+{
+	while(1);
+}
