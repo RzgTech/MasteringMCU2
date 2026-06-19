51cab2ecfd85afccbafb545a60efb6da53960d5e
Vahid <vahidrspace@gmail.com>
2026-06-19 21:36:57 +0200

    project creation for WFE_button exercise

---
+++ WFE_button/Core/Src/data.c
@@ -0,0 +1,11 @@
+/*
+ * data.c
+ *
+ *  Created on: Jun 14, 2026
+ *      Author: Vahid
+ */
+
+#include "main_app.h"
+
+//some randomly generated text
+char some_data[] = "We are testing WFI Instruction\r\n";
---
+++ WFE_button/Core/Src/main_app.c
@@ -0,0 +1,222 @@
+/*
+ * main.c
+ *
+ *  Created on: Jun 8, 2026
+ *      Author: Vahid
+ */
+
+#include "main_app.h"
+#include "stm32f4xx_hal.h"
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+
+void SystemCLock_Config_HSE(uint8_t clocl_freq);
+void GPIO_Init(void);
+void UART2_Init();
+void Error_handler(void);
+void GPIO_AnalogConfig(void);
+
+UART_HandleTypeDef huart;  //UART2 handle
+extern uint8_t some_data[];
+
+int main()
+{
+
+	char msg[50];
+	HAL_Init();
+	//SystemCLock_Config_HSE(SYS_CLOCK_FREQ_50_MHZ);  we cooment this to use the HSI clock which is the default clock: 16MHz
+
+	GPIO_Init();
+	UART2_Init();
+
+	GPIO_AnalogConfig();
+
+	while(1)
+	{
+		//going to sleep
+		__WFI();
+		//MCU resumes here when it wakes up
+	}
+
+	return 0;
+}
+
+void SystemCLock_Config_HSE(uint8_t clocl_freq)
+{
+	RCC_OscInitTypeDef osc_init;
+	RCC_ClkInitTypeDef clk_init;
+
+	uint32_t FLatency = 0;
+
+	osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	osc_init.HSEState = RCC_HSE_BYPASS;
+	osc_init.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	osc_init.PLL.PLLState = RCC_PLL_ON;
+
+	switch (clocl_freq)
+	{
+	case SYS_CLOCK_FREQ_50_MHZ:
+	{
+		osc_init.PLL.PLLM = 4;
+		osc_init.PLL.PLLN = 50;
+		osc_init.PLL.PLLP = RCC_PLLP_DIV2;
+		osc_init.PLL.PLLQ = 2; //default value
+		osc_init.PLL.PLLR = 2; //default value
+
+		clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+				RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+		clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+		clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+		clk_init.APB2CLKDivider = RCC_HCLK_DIV1;
+
+		FLatency = FLASH_ACR_LATENCY_1WS;
+		break;
+	}
+	case SYS_CLOCK_FREQ_84_MHZ:
+	{
+		osc_init.PLL.PLLM = 4;
+		osc_init.PLL.PLLN = 84;
+		osc_init.PLL.PLLP = RCC_PLLP_DIV2;
+		osc_init.PLL.PLLQ = 2; //default value
+		osc_init.PLL.PLLR = 1; //default value
+
+		clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+				RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+		clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+		clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+		clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+
+		FLatency = FLASH_ACR_LATENCY_2WS;
+		break;
+	}
+	case SYS_CLOCK_FREQ_120_MHZ:
+	{
+		osc_init.PLL.PLLM = 4;
+		osc_init.PLL.PLLN = 120;
+		osc_init.PLL.PLLP = RCC_PLLP_DIV2;
+		osc_init.PLL.PLLQ = 4; //default value
+		osc_init.PLL.PLLR = 2; //default value
+
+
+		clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+				RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+		clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+		clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		clk_init.APB1CLKDivider = RCC_HCLK_DIV4;
+		clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+
+		FLatency = FLASH_ACR_LATENCY_3WS;
+		break;
+	}
+	default:
+		return;
+	}
+
+	if (HAL_RCC_OscConfig(&osc_init)!= HAL_OK)
+	{
+		Error_handler();
+	}
+
+	if (HAL_RCC_ClockConfig(&clk_init, FLatency) != HAL_OK)
+	{
+		Error_handler();
+	}
+
+	//Systick configuration
+	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+	HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+void GPIO_Init(void)
+{
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_SLEEP_DISABLE(); //disabling GPIOA clock when it is in sleep mode. we cannot do the same for GPIOC bcs it is our wake up source
+    GPIO_InitTypeDef ledgpio, buttongpio;
+#if 0
+    ledgpio.Pin = GPIO_PIN_12;
+    ledgpio.Mode = GPIO_MODE_OUTPUT_PP;
+    ledgpio.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA,&ledgpio);
+#endif
+    buttongpio.Pin = GPIO_PIN_13;
+    buttongpio.Mode = GPIO_MODE_IT_FALLING;
+    buttongpio.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOC,&buttongpio);
+
+    HAL_NVIC_SetPriority(EXTI15_10_IRQn, 15, 0);
+    HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+
+}
+
+/**
+  * @brief  Configures specified GPIO pins of GPIOA and GPIOC ports as analog.
+  * @retval None
+  */
+void GPIO_AnalogConfig(void)
+{
+  GPIO_InitTypeDef GpioA,GpioC;
+
+  //skip GPIO 13 and 14 as they are SWDIO and SWD_CLK
+  uint32_t gpio_pins = GPIO_PIN_0  | GPIO_PIN_1  | GPIO_PIN_4  | \
+                       GPIO_PIN_5  | GPIO_PIN_6  | GPIO_PIN_7  | \
+                       GPIO_PIN_8  | GPIO_PIN_9  | GPIO_PIN_10 | \
+                       GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_15;
+
+  GpioA.Pin = gpio_pins;
+  GpioA.Mode = GPIO_MODE_ANALOG;
+  HAL_GPIO_Init(GPIOA,&GpioA);
+
+  gpio_pins = GPIO_PIN_0  | GPIO_PIN_1  | GPIO_PIN_2  | \
+              GPIO_PIN_3  | GPIO_PIN_4  | GPIO_PIN_5  | \
+              GPIO_PIN_6  | GPIO_PIN_7  | GPIO_PIN_8  | \
+              GPIO_PIN_9  | GPIO_PIN_10 | GPIO_PIN_11 | \
+              GPIO_PIN_12 | GPIO_PIN_14 | GPIO_PIN_15;
+
+  GpioC.Pin = gpio_pins;
+  GpioC.Mode = GPIO_MODE_ANALOG;
+  HAL_GPIO_Init(GPIOC,&GpioC);
+}
+
+void UART2_Init(void)
+{
+	huart.Instance = USART2;
+	huart.Init.BaudRate = 115200;
+	huart.Init.WordLength = UART_WORDLENGTH_8B;
+	huart.Init.StopBits = UART_STOPBITS_1;
+	huart.Init.Parity = UART_PARITY_NONE;
+	huart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart.Init.Mode = UART_MODE_TX_RX;
+
+	if (HAL_UART_Init(&huart) != HAL_OK)
+	{
+		//there is a problem
+		Error_handler();
+	}
+}
+
+/**
+  * @brief  EXTI line detection callbacks.
+  * @param  GPIO_Pin Specifies the pins connected EXTI line
+  * @retval None
+  */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+    if ( HAL_UART_Transmit(&huart,(uint8_t*)some_data,(uint16_t)strlen((char*)some_data),HAL_MAX_DELAY) != HAL_OK)
+    {
+	  Error_handler();
+    }
+}
+
+void Error_handler(void)
+{
+	while(1);
+}
